043510343327e92dc17f1d4114c831ad6d8f215c
Aysel Abiyeva <ayselabiyeffam13@gmail.com>
2025-09-15 07:44:13 +0000

    Aysel Abiyeva Lab submit 231ADB279

--- src/hello.c
+++ src/hello.c
@@ -2,8 +2,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  // This is your first C program my friend
-  printf("Hello, RTU World from C Lab in 2025!\n");
+  // This is my first C program after 2 years!
+  printf("Hello, RTU World from C Lab in 2025 Aysel Abiyeva was here (231ADB279) :) !\n");
   printf("You passed %d argument(s).\n", argc - 1);
   for (int i = 1; i < argc; ++i) {
     printf("  arg[%d] = %s\n", i, argv[i]);
